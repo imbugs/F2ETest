81bf544db542feb999ac1d949b2209d69f6488ab
unknown <Administrator@.(none)>
2012-03-26 15:19:18 +0800

    1

--- server/API/doRequest.js
+++ server/API/doRequest.js
@@ -1,26 +1,39 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
-    console.log(request.url);
+    console.log('123'+request.url);
 
-    if(request.url != 'doRequest'){
+    //只接受这个请求
+    if(request.url != '/doRequest'){
         response.writeHead(404, {"Content-Type": "text/plain"});
         response.end();
+        return;
     }
+    //GET请求由于数据量肯呢过会很大，不支持它
+//    if(request.method == 'GET'){
+//        response.writeHead(405, {"Content-Type": "text/plain"});
+//        response.write("请使用POST方法！");
+//        response.end();
+//        return;
+//    }
 
+    //获取参数
+    var data = querystring.parse(request.url);
+    console.log(data);
     response.writeHead(200, {"Content-Type": "text/plain"});
     var webdriverjs = require("webdriverjs");
 
     var client = webdriverjs.remote({
         'host': '10.13.15.49',
+        'port': 4444,
         "desiredCapabilities":{
             //"browserName":"internet explorer"
         }
     });
 
-    var tmp = client.showTest;
+    var showTest = client.showTest;
     client.showTest = function(){
-        tmp.apply( webdriverjs, arguments);
+        showTest.apply( webdriverjs, arguments);
         response.write('123');
         response.write(arguments[1]);
     }
--- server/writable/cases/1.js
+++ server/writable/cases/1.js
@@ -2,12 +2,12 @@ exports.run = function(client, response){
     client
         .init()
         .url("http://www.google.com")
-        .setValue("#lst-ib", "webdriver")
-        .submitForm("#tsf")
-        .getTitle(function(e){
-            response.write(e);
-        })
-        .tests.titleEquals("Foo", "Title of the page is 'Foo'")
+//        .setValue("#lst-ib", "webdriver")
+//        .submitForm("#tsf")
+//        .getTitle(function(e){
+//            response.write(e);
+//        })
+        .tests.titleEquals("Google", "Title of the page is 'Google'")
 //        .saveScreenshot("1.jpg",function(e){
 //            console.log( '123' );
 //            console.log( e );
